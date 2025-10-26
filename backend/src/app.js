8131b3aba60467cc8e038d30f75c283dfb834e41
eduardo <198397564+esb-11@users.noreply.github.com>
2025-10-26 17:30:50 -0300

    feat: handle file input

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,19 @@
 import express from "express";
+import router from "./routes/router.js";
+import cors from "cors";
 
-const PORT = 5500;
+const PORT = 3390;
 const app = express();
+
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use("/", express.text({ limit: "10mb" }));
+app.use("/", router);
 
 app.listen(PORT, () => {
   console.log(`Listening to port ${PORT}`);
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,34 @@
 import "./App.css";
 
 function App() {
+  async function handleFile(e) {
+    const file = e.target.files[0];
+    const response = await fetch("http://192.168.1.6:3390/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "text/plain",
+      },
+      credentials: "include",
+      body: file,
+    });
+    console.log(response);
+  }
+
   return (
     <>
       <h1>Planilha de Inventario</h1>
+      <div>
+        <label htmlFor="input-file">
+          <span>Selecione o arquivo</span>
+          <input
+            onChange={handleFile}
+            type="file"
+            name="input-file"
+            id="input-file"
+            accept=".csv"
+          />
+        </label>
+      </div>
     </>
   );
 }
